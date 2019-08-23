210c12e6e7690351f6a0832cba8260decb5674ef
Daw-Ran Liou <dawran6@gmail.com>
2019-08-23 09:57:28 -0700

    Add basic tests

--- test/assets/core_test.clj
+++ test/assets/core_test.clj
@@ -1,12 +1,38 @@
 (ns assets.core-test
-  (:require [assets.core :as assets]
-            [clojure.test :refer [deftest testing is]]))
+  (:require [assets.core :refer [assets-dir ext hrefs md5 paths]]
+            [clojure.test :refer [deftest is testing]]))
 
 (def assets-edn
   {"bundle.css" ["tachyons.min.css" "app.css"]
    "bundle.js"  ["app.js"]})
 
+(deftest test-md5
+  (testing "same md5 digest"
+    (is (= (md5 "xxx")
+           (md5 "xxx")))
+    (is (= (md5 "yyy")
+           (md5 "yyy"))))
+  (testing "different md5 digest"
+    (is (not= (md5 "xxx")
+              (md5 "yyy")))))
+
+(deftest test-ext
+  (testing "file with extension"
+    (is (= (ext "app.css") "css"))
+    (is (= (ext "app.js") "js")))
+  (testing "file without extension"
+    (is (nil? (ext "app")))))
+
+(deftest test-hrefs
+  (testing "basic case"
+    (is (= (hrefs "css" ["a.css" "b.css"])
+           ["/css/a.css" "/css/b.css"]))))
+
 (deftest test-paths
-  (testing "test basic case"
-    (is (assets/paths "css" ["a.css" "b.css"])
+  (testing "basic case"
+    (is (paths "css" ["a.css" "b.css"])
         ["resources/public/css/a.css" "resources/public/css/b.css"])))
+
+(deftest test-assets-dir
+  (testing "basic case"
+    (is (assets-dir) "resources/public/assets")))
